16d4f9aadf2db9f0e91571b490ba4190541a2d11
Sergio Campbell <51247757+SergioCampbell@users.noreply.github.com>
2020-09-10 12:43:00 -0500

    Ecam
    Usinging camera from smartphone with IONIC and AngularJS

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions} from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  foto: any;
+  constructor(private camera: Camera) {}
+
+  TomarFoto(){
+
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      this.foto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
